12ba2e815f4fe8a16d35beb5ef77e63b53326d05
Your Name <you@example.com>
2021-08-29 19:35:48 +0800

    config params in build.gradle for image plugs

--- buildSrc/src/main/groovy/com/darren/optimize/plugin/ImageMonitorPlugin.groovy
+++ buildSrc/src/main/groovy/com/darren/optimize/plugin/ImageMonitorPlugin.groovy
@@ -5,12 +5,13 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ImageMonitorPlugin implements Plugin<Project> {
+    // 与app/build.gradle 配置参数一致
     public static final String EXT_NAME = "tinkerPatch"
     @Override
     void apply(Project project) {
-        // 传递参数
-//        project.extensions.create(EXT_NAME, TinkerPatchParams)
-        println "Hello Plugin: " + project
-        // 这里怎么写？
+        // 从app/build.gradle 传递参数tinkerPatch 到插件处理
+        println "Hello Plugin wwwj: " + project
+        TinkerPatchParams tinkerPatchParams = project.extensions.create(EXT_NAME, TinkerPatchParams)
+        println "oldApk: " + tinkerPatchParams.oldApk
     }
 }
